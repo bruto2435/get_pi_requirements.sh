fa1ccb60fe1cf37e05e9157dc9dbf6313082b71c
bruto2435 <165106796+bruto2435@users.noreply.github.com>
2024-03-27 08:01:05 +0700

    Update get_pi_requirements.sh

--- get_pi_requirements.sh
+++ get_pi_requirements.sh
@@ -1,1 +1,33 @@
+#!/bin/bash
+
+# Get packages required for OpenCV
+sudo apt-get -y install libjpeg-dev libtiff5-dev libjasper-dev libpng-dev
+sudo apt-get -y install libavcodec-dev libavformat-dev libswscale-dev libv4l-dev
+sudo apt-get -y install libxvidcore-dev libx264-dev
+sudo apt-get -y install qt4-dev-tools 
+sudo apt-get -y install libatlas-base-dev
+
+# Need to get an older version of OpenCV because version 4 has errors
+pip3 install opencv-python==3.4.11.41
+
+# Get packages required for TensorFlow Lite runtime
+# Using the tflite_runtime packages available at https://www.tensorflow.org/lite/guide/python
+
+# Determine Python version
+version=$(python3 -c 'import sys; print(".".join(map(str, sys.version_info[:2])))')
+
+# Install TensorFlow Lite runtime based on Python version
+if [ $version == "3.9" ]; then
+    pip3 install https://github.com/google-coral/pycoral/releases/download/v2.0.0/tflite_runtime-2.5.0.post1-cp39-cp39-linux_armv7l.whl
+elif [ $version == "3.8" ]; then
+    pip3 install https://github.com/google-coral/pycoral/releases/download/v2.0.0/tflite_runtime-2.5.0.post1-cp38-cp38-linux_armv7l.whl
+elif [ $version == "3.7" ]; then
+    pip3 install https://github.com/google-coral/pycoral/releases/download/v2.0.0/tflite_runtime-2.5.0.post1-cp37-cp37m-linux_armv7l.whl
+elif [ $version == "3.6" ]; then
+    pip3 install https://github.com/google-coral/pycoral/releases/download/v2.0.0/tflite_runtime-2.5.0.post1-cp36-cp36m-linux_armv7l.whl
+elif [ $version == "3.5" ]; then
+    pip3 install https://github.com/google-coral/pycoral/releases/download/release-frogfish/tflite_runtime-2.5.0-cp35-cp35m-linux_armv7l.whl
+else
+    echo "Python version $version is not supported for TensorFlow Lite runtime."
+fi
 
